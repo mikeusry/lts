af1c2af6da5dd1b9f8568e55651a8d227b04cfe0
mikeusry <mikeusry@gmail.com>
2012-10-31 04:04:26 -0700

    d

---
+++ lab_test/js/js_9aevSzD2vNmFr0q77nWgPOD_fpPt2NvlshQGtXD3LdI.js
@@ -0,0 +1,364 @@
+(function ($) {
+
+/**
+ * Attaches sticky table headers.
+ */
+Drupal.behaviors.tableHeader = {
+  attach: function (context, settings) {
+    if (!$.support.positionFixed) {
+      return;
+    }
+
+    $('table.sticky-enabled', context).once('tableheader', function () {
+      $(this).data("drupal-tableheader", new Drupal.tableHeader(this));
+    });
+  }
+};
+
+/**
+ * Constructor for the tableHeader object. Provides sticky table headers.
+ *
+ * @param table
+ *   DOM object for the table to add a sticky header to.
+ */
+Drupal.tableHeader = function (table) {
+  var self = this;
+
+  this.originalTable = $(table);
+  this.originalHeader = $(table).children('thead');
+  this.originalHeaderCells = this.originalHeader.find('> tr > th');
+  this.displayWeight = null;
+
+  // React to columns change to avoid making checks in the scroll callback.
+  this.originalTable.bind('columnschange', function (e, display) {
+    // This will force header size to be calculated on scroll.
+    self.widthCalculated = (self.displayWeight !== null && self.displayWeight === display);
+    self.displayWeight = display;
+  });
+
+  // Clone the table header so it inherits original jQuery properties. Hide
+  // the table to avoid a flash of the header clone upon page load.
+  this.stickyTable = $('<table class="sticky-header"/>')
+    .insertBefore(this.originalTable)
+    .css({ position: 'fixed', top: '0px' });
+  this.stickyHeader = this.originalHeader.clone(true)
+    .hide()
+    .appendTo(this.stickyTable);
+  this.stickyHeaderCells = this.stickyHeader.find('> tr > th');
+
+  this.originalTable.addClass('sticky-table');
+  $(window)
+    .bind('scroll.drupal-tableheader', $.proxy(this, 'eventhandlerRecalculateStickyHeader'))
+    .bind('resize.drupal-tableheader', { calculateWidth: true }, $.proxy(this, 'eventhandlerRecalculateStickyHeader'))
+    // Make sure the anchor being scrolled into view is not hidden beneath the
+    // sticky table header. Adjust the scrollTop if it does.
+    .bind('drupalDisplaceAnchor.drupal-tableheader', function () {
+      window.scrollBy(0, -self.stickyTable.outerHeight());
+    })
+    // Make sure the element being focused is not hidden beneath the sticky
+    // table header. Adjust the scrollTop if it does.
+    .bind('drupalDisplaceFocus.drupal-tableheader', function (event) {
+      if (self.stickyVisible && event.clientY < (self.stickyOffsetTop + self.stickyTable.outerHeight()) && event.$target.closest('sticky-header').length === 0) {
+        window.scrollBy(0, -self.stickyTable.outerHeight());
+      }
+    })
+    .triggerHandler('resize.drupal-tableheader');
+
+  // We hid the header to avoid it showing up erroneously on page load;
+  // we need to unhide it now so that it will show up when expected.
+  this.stickyHeader.show();
+};
+
+/**
+ * Event handler: recalculates position of the sticky table header.
+ *
+ * @param event
+ *   Event being triggered.
+ */
+Drupal.tableHeader.prototype.eventhandlerRecalculateStickyHeader = function (event) {
+  var self = this;
+  var calculateWidth = event.data && event.data.calculateWidth;
+
+  // Reset top position of sticky table headers to the current top offset.
+  this.stickyOffsetTop = Drupal.settings.tableHeaderOffset ? eval(Drupal.settings.tableHeaderOffset + '()') : 0;
+  this.stickyTable.css('top', this.stickyOffsetTop + 'px');
+
+  // Save positioning data.
+  var viewHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+  if (calculateWidth || this.viewHeight !== viewHeight) {
+    this.viewHeight = viewHeight;
+    this.vPosition = this.originalTable.offset().top - 4 - this.stickyOffsetTop;
+    this.hPosition = this.originalTable.offset().left;
+    this.vLength = this.originalTable[0].clientHeight - 100;
+    calculateWidth = true;
+  }
+
+  // Track horizontal positioning relative to the viewport and set visibility.
+  var hScroll = document.documentElement.scrollLeft || document.body.scrollLeft;
+  var vOffset = (document.documentElement.scrollTop || document.body.scrollTop) - this.vPosition;
+  this.stickyVisible = vOffset > 0 && vOffset < this.vLength;
+  this.stickyTable.css({ left: (-hScroll + this.hPosition) + 'px', visibility: this.stickyVisible ? 'visible' : 'hidden' });
+
+  // Only perform expensive calculations if the sticky header is actually
+  // visible or when forced.
+  if (this.stickyVisible && (calculateWidth || !this.widthCalculated)) {
+    this.widthCalculated = true;
+    var $that = null;
+    var $stickyCell = null;
+    var display = null;
+    var cellWidth = null;
+    // Resize header and its cell widths.
+    // Only apply width to visible table cells. This prevents the header from
+    // displaying incorrectly when the sticky header is no longer visible.
+    for (var i = 0, il = this.originalHeaderCells.length; i < il; i += 1) {
+      $that = $(this.originalHeaderCells[i]);
+      $stickyCell = this.stickyHeaderCells.eq($that.index());
+      display = $that.css('display');
+      if (display !== 'none') {
+        cellWidth = $that.css('width');
+        // Exception for IE7.
+        if (cellWidth === 'auto') {
+          cellWidth = $that[0].clientWidth + 'px';
+        }
+        $stickyCell.css({'width': cellWidth, 'display': display});
+      }
+      else {
+        $stickyCell.css('display', 'none');
+      }
+    }
+    this.stickyTable.css('width', this.originalTable.css('width'));
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the uri fragment identifier. 
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($('.error' + anchor, $fieldset).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+  Drupal.behaviors.vbo = {
+    attach: function(context) {
+      $('.vbo-views-form', context).each(function() {
+        Drupal.vbo.initTableBehaviors(this);
+        Drupal.vbo.initGenericBehaviors(this);
+      });
+    }
+  }
+
+  Drupal.vbo = Drupal.vbo || {};
+  Drupal.vbo.initTableBehaviors = function(form) {
+    // If the table is not grouped, "Select all on this page / all pages"
+    // markup gets inserted below the table header.
+    var selectAllMarkup = $('.vbo-table-select-all-markup', form);
+    if (selectAllMarkup.length) {
+      $('.views-table > tbody', form).prepend('<tr class="views-table-row-select-all even">></tr>');
+      var colspan = $('table th', form).length;
+      $('.views-table-row-select-all', form).html('<td colspan="' + colspan + '">' + selectAllMarkup.html() + '</td>');
+
+      $('.vbo-table-select-all-pages', form).click(function() {
+        Drupal.vbo.tableSelectAllPages(form);
+        return false;
+      });
+      $('.vbo-table-select-this-page', form).click(function() {
+        Drupal.vbo.tableSelectThisPage(form);
+        return false;
+      });
+    }
+
+    $('.vbo-table-select-all', form).show();
+    // This is the "select all" checkbox in (each) table header.
+    $('.vbo-table-select-all', form).click(function() {
+      var table = $(this).closest('table')[0];
+      $('input[id^="edit-views-bulk-operations"]:not(:disabled)', table).attr('checked', this.checked);
+
+      // Toggle the visibility of the "select all" row (if any).
+      if (this.checked) {
+        $('.views-table-row-select-all', table).show();
+      }
+      else {
+        $('.views-table-row-select-all', table).hide();
+        // Disable "select all across pages".
+        Drupal.vbo.tableSelectThisPage(form);
+      }
+    });
+
+    // Set up the ability to click anywhere on the row to select it.
+    $('.views-table tbody tr', form).click(function(event) {
+      if (event.target.tagName.toLowerCase() != 'input' && event.target.tagName.toLowerCase() != 'a') {
+        $('input[id^="edit-views-bulk-operations"]:not(:disabled)', this).each(function() {
+          var checked = this.checked;
+          // trigger() toggles the checkmark *after* the event is set,
+          // whereas manually clicking the checkbox toggles it *beforehand*.
+          // that's why we manually set the checkmark first, then trigger the
+          // event (so that listeners get notified), then re-set the checkmark
+          // which the trigger will have toggled. yuck!
+          this.checked = !checked;
+          $(this).trigger('click');
+          this.checked = !checked;
+        });
+      }
+    });
+  }
+
+  Drupal.vbo.tableSelectAllPages = function(form) {
+    $('.vbo-table-this-page', form).hide();
+    $('.vbo-table-all-pages', form).show();
+    // Modify the value of the hidden form field.
+    $('.select-all-rows', form).val('1');
+  }
+  Drupal.vbo.tableSelectThisPage = function(form) {
+    $('.vbo-table-all-pages', form).hide();
+    $('.vbo-table-this-page', form).show();
+    // Modify the value of the hidden form field.
+    $('.select-all-rows', form).val('0');
+  }
+
+  Drupal.vbo.initGenericBehaviors = function(form) {
+    // Show the "select all" fieldset.
+    $('.vbo-select-all-markup', form).show();
+
+    $('.vbo-select-this-page', form).click(function() {
+      $('input[id^="edit-views-bulk-operations"]', form).attr('checked', this.checked);
+      $('.vbo-select-all-pages', form).attr('checked', false);
+
+      // Toggle the "select all" checkbox in grouped tables (if any).
+      $('.vbo-table-select-all', form).attr('checked', this.checked);
+    });
+    $('.vbo-select-all-pages', form).click(function() {
+      $('input[id^="edit-views-bulk-operations"]', form).attr('checked', this.checked);
+      $('.vbo-select-this-page', form).attr('checked', false);
+
+      // Toggle the "select all" checkbox in grouped tables (if any).
+      $('.vbo-table-select-all', form).attr('checked', this.checked);
+
+      // Modify the value of the hidden form field.
+      $('.select-all-rows', form).val(this.checked);
+    });
+
+    $('.vbo-select', form).click(function() {
+      // If a checkbox was deselected, uncheck any "select all" checkboxes.
+      if (!this.checked) {
+        $('.vbo-select-this-page', form).attr('checked', false);
+        $('.vbo-select-all-pages', form).attr('checked', false);
+        // Modify the value of the hidden form field.
+        $('.select-all-rows', form).val('0')
+
+        var table = $(this).closest('table')[0];
+        if (table) {
+          // Uncheck the "select all" checkbox in the table header.
+          $('.vbo-table-select-all', table).attr('checked', false);
+
+          // If there's a "select all" row, hide it.
+          if ($('.vbo-table-select-this-page', table).length) {
+            $('.views-table-row-select-all', table).hide();
+            // Disable "select all across pages".
+            Drupal.vbo.tableSelectThisPage(form);
+          }
+        }
+      }
+    });
+  }
+
+})(jQuery);
+;
